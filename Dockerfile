8f2cb8ac2d0301b4fec5cee3d26d008a33d1d5b0
vishalranjile <vishalranjile12@gmail.com>
2024-01-05 19:16:42 +0530

    y

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+from node:18
+WORKDIR/app
+COPY package.json/app
+RUN npm install
+COPY ./app
+CMD node server.js
+EXPOSE 5001
---
+++ server.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var app=express();
+app.use(express.static('public'));
+app.get("/",(req,res)=>{res.send("welcome");});
+app.listen(5001);
+console.log("listening to 5001");
